f4488eed92ce68c04995551546e7f894daf65dd2
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-26 18:35:20 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+
+    validates :content, length: {minimum: 250}
+
+    validates :summary, length: {maximum: 250}
+
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    # validates :clickbait?
+
+    # private
+
+    # def clickbait
+    #     if title.present? && (!title.include?("You Won't Believe The True Facts") && !title.include?("Secret") && !title.include?("Top /\d/") && !title.include?("Guess"))
+    #         errors.add(:title, "must bait clicks")
+    #     end
+    # end
 end
